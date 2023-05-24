f7921246ec6691c32bb01b881ccdc7749b0acabc
trevor wells <104236134+trevor-wells@users.noreply.github.com>
2023-05-24 12:44:25 -0600

    final commit

--- server/app.py
+++ server/app.py
@@ -17,7 +17,36 @@ db.init_app(app)
 api = Api(app)
 
 class Home(Resource):
-    pass
+    def get(self):
+        response_dict = {"message": "Welcome to the Newsletter RESTful API"}
+
+        response = make_response(response_dict, 200)
+        return response
+
+class Newsletters(Resource):
+    def get(self):
+        response_dict_list = [n.to_dict() for n in Newsletter.query.all()]
+        response = make_response(response_dict_list, 200)
+        return response
+    
+    def post(self):
+        new_record = Newsletter(
+            title = request.form['title'],
+            body  = request.form['body']
+        )
+        db.session.add(new_record)
+        db.session.commit()
+        response = make_response(new_record.to_dict(), 201)
+        return response
+    
+class NewsletterByID(Resource):
+    def get(self, id):
+        response_dict = Newsletter.query.filter_by(id=id).first().to_dict()
+        return make_response(response_dict, 200)
+
+api.add_resource(Home, '/')
+api.add_resource(Newsletters, '/newsletters')
+api.add_resource(NewsletterByID, '/newsletters/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
